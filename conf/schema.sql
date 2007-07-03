1bda998872213e7013ce56d6f63280228fc6c525
beuss <beuss@6b1a4e3b-4c34-0410-9386-843e98abba4f>
2007-07-03 08:23:47 +0000

    Added indices on user_group. Speed up is awesone (7min lookup with several thousands of groups vs 1 second).

--- conf/schema.sql
+++ conf/schema.sql
@@ -1,6 +1,10 @@
 CREATE TABLE shadow(uid INTEGER PRIMARY KEY, gid INTEGER, username TEXT NOT NULL, passwd TEXT NOT NULL, gecos TEXT NOT NULL default '', shell TEXT NOT NULL,  homedir TEXT NOT NULL);
 CREATE INDEX idx_username ON shadow(username);
+
 CREATE TABLE user_group(uid INTEGER, gid INTEGER, CONSTRAINT pk_user_groups PRIMARY KEY(uid, gid));
+CREATE INDEX idx_ug_uid ON user_group(uid);
+CREATE INDEX idx_ug_gid ON user_group(gid);
+
 CREATE TABLE groups(gid INTEGER PRIMARY KEY, groupname TEXT NOT NULL, passwd TEXT NOT NULL DEFAULT '');
 CREATE INDEX idx_groupname ON groups(groupname);
 
